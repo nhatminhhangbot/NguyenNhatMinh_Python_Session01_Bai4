3b1e5d5cfd953db17a10a7280af7b707fa91e6b4
nhatminhhangbot <nhatminh.hangbot@gmail.com>
2026-05-18 17:45:55 +0700

    Initial commit

---
+++ Bai4.py
@@ -0,0 +1,12 @@
+patient_id = input("Nhập mã bệnh nhân (VD: BN999): ")
+temperature = float(input("Nhập nhiệt độ cơ thể: "))
+heart_rate = int(input("Nhập nhịp tim: "))
+
+print("--- KẾT QUẢ CHUẨN HÓA DỮ LIỆU ---")
+print("Mã bệnh nhân:", patient_id)
+print("Nhiệt độ cơ thể:", temperature, "độ C")
+print("=> Kiểu dữ liệu hệ thống ghi nhận:", type(temperature))
+print("Nhịp tim:", heart_rate, "nhịp/phút")
+print("=> Kiểu dữ liệu hệ thống ghi nhận:", type(heart_rate))
+print("---------------------------------")
+print("Thông báo: Dữ liệu hợp lệ. Màn hình Monitor đã sẵn sàng kết nối!")
